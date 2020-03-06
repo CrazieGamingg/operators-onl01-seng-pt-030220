3dcb6514513a351c96da970bae56d58ae430fa23
Edward Lindsey <eddielindsey23@gmail.com>
2020-03-06 22:53:55 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,9 @@
+## INPUTS --> speed --> integer
+## OUTPUTS --> answer the question am i unsafe --> boolean
+
+
+# if your speed is > 60 or your speed is < 40 say true
+# otherwise, say false
 def unsafe?(speed)
 
 end
